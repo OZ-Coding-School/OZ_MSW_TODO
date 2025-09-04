a64d311355d9010acd680c43cb5a6d793a16b248
강성구 <sungku2757@gmail.com>
2025-09-04 22:28:58 +0900

    fix: renewer landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import "./App.css";
+import TodoList from "./components/Todo/TodoList";
+import { useTodoHandler } from "./hooks/todo/useTodoHandler";
+import styles from "./App.module.css";
+import Navbar from "./components/nav/Navbar";
+import AddTodoSection from "./components/todo/AddTodoSection";
+
+const TODOs = [
+  { todoId: 1, content: "프론트", isFinish: false },
+  { todoId: 2, content: "프론트", isFinish: true },
+  { todoId: 3, content: "프론트", isFinish: false },
+];
 
 function App() {
-  return <></>;
+  const { todos, addTodo, finishTodo } = useTodoHandler();
+
+  return (
+    <div className={styles.app}>
+      <Navbar />
+      <AddTodoSection addTodo={addTodo} />
+      <TodoList todos={TODOs} finishTodo={finishTodo} />
+    </div>
+  );
 }
 
 export default App;
