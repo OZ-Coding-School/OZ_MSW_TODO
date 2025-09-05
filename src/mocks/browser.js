51a0ba65a120e0aa2b7939a2c9fe531471ab72e3
L1m3Kun <sungku2757@gmail.com>
2025-09-05 10:45:53 +0900

    feat: todos 가져오기 msw 설정 추가

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -1,1 +1,19 @@
-export const handlers = [];
+import { HttpResponse, http } from "msw";
+
+const todos = [
+  { todoId: 1, content: "프론트", isFinish: false },
+  { todoId: 2, content: "프론트", isFinish: true },
+  { todoId: 3, content: "프론트", isFinish: false },
+];
+
+export const handlers = [
+  // 가져오기
+  http.get("/todo", () => {
+    return HttpResponse.json(
+      {
+        todos,
+      },
+      { status: 200 }
+    );
+  }),
+];
